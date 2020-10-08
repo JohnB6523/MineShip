6e45ff98a537de24fc52f31c4fbb8d77767000b5
John Bell <2206568B@student.gla.ac.uk>
2020-10-08 15:37:56 +0100

    Converted main page to fragment for navigation

---
+++ app/src/main/java/io/github/johnb6523/mineship/GamePage.kt
@@ -0,0 +1,107 @@
+package io.github.johnb6523.mineship
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.core.content.res.ResourcesCompat
+
+val smallPieces = listOf(Tile.SHIP_S1, Tile.SHIP_S2)
+val medPieces = listOf(Tile.SHIP_M1, Tile.SHIP_M2, Tile.SHIP_M3)
+val largePieces = listOf(Tile.SHIP_L1, Tile.SHIP_L2, Tile.SHIP_L3, Tile.SHIP_L4)
+val allPieces = listOf(Tile.SHIP_S1, Tile.SHIP_S2, Tile.SHIP_M1, Tile.SHIP_M2, Tile.SHIP_M3,
+    Tile.SHIP_L1, Tile.SHIP_L2, Tile.SHIP_L3, Tile.SHIP_L4)
+
+class RulesPage : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val newGameButton = view.findViewById<Button>(R.id.newGameButton)
+        newGameButton.setOnClickListener {
+            initGame(view)
+        }
+        initGame(view)
+    }
+
+    private fun initGame(view: View) {
+        val state = GameState()
+        val table = view.findViewById<TableLayout>(R.id.gridTable)
+        for (i in 0..7) {
+            val row = table.getChildAt(i) as TableRow
+            for (j in 0..7) {
+                val tile = row.getChildAt(j) as ImageView
+                tile.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.tile_unrevealed, null))
+                tile.rotation = 0F
+                tile.setOnClickListener {
+                    showTile(i,j,tile,state)
+                    revealTile(view, i,j,state)
+                }
+            }
+        }
+        val ship2icon = view.findViewById<ImageView>(R.id.ship2Icon)
+        ship2icon.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.icon_ship_2, null))
+        val ship3icon = view.findViewById<ImageView>(R.id.ship3Icon)
+        ship3icon.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.icon_ship_3, null))
+        val ship4icon = view.findViewById<ImageView>(R.id.ship4Icon)
+        ship4icon.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.icon_ship_4, null))
+        val text = view.findViewById<TextView>(R.id.textView)
+        text.setText(R.string.app_name)
+    }
+
+    // revealTile handles gameplay logic of revealing a tapped tile
+    private fun revealTile(view: View, i : Int, j : Int, state : GameState) {
+        when (state.gridContents[i][j]) {
+            Tile.MINE -> endGame(view, state, false)
+            in allPieces -> state.foundPieces.add(state.gridContents[i][j])
+        }
+        if (state.foundPieces.containsAll(smallPieces)) {
+            val ship2icon = view.findViewById<ImageView>(R.id.ship2Icon)
+            ship2icon.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.icon_ship_destroyed, null))
+        }
+        if (state.foundPieces.containsAll(medPieces)) {
+            val ship3icon = view.findViewById<ImageView>(R.id.ship3Icon)
+            ship3icon.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.icon_ship_destroyed, null))
+        }
+        if (state.foundPieces.containsAll(largePieces)) {
+            val ship4icon = view.findViewById<ImageView>(R.id.ship4Icon)
+            ship4icon.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.icon_ship_destroyed, null))
+        }
+        if (state.foundPieces.containsAll(allPieces)) {
+            endGame(view, state, true)
+        }
+    }
+
+    // showTile handles graphics only, is used when tapped and on showing full board on win/loss
+    private fun showTile(i : Int, j : Int, tile : ImageView, state : GameState) {
+        tile.setImageDrawable(ResourcesCompat.getDrawable(resources, state.gridContents[i][j].image, null))
+        when (state.gridContents[i][j]) {
+            in smallPieces -> tile.rotation = state.ship2Ori * 90F
+            in medPieces -> tile.rotation = state.ship3Ori * 90F
+            in largePieces -> tile.rotation = state.ship4Ori * 90F
+        }
+    }
+
+    private fun endGame(view: View, state : GameState, win : Boolean) {
+        val table = view.findViewById<TableLayout>(R.id.gridTable)
+        for (i in 0..7) {
+            val row = table.getChildAt(i) as TableRow
+            for (j in 0..7) {
+                val tile = row.getChildAt(j) as ImageView
+                showTile(i,j,tile,state)
+                tile.setOnClickListener(null)
+            }
+        }
+        val text = view.findViewById<TextView>(R.id.textView)
+        text.setText(if (win) R.string.win else R.string.lose)
+    }
+}
--- app/src/main/java/io/github/johnb6523/mineship/MainActivity.kt
+++ app/src/main/java/io/github/johnb6523/mineship/MainActivity.kt
@@ -6,94 +6,12 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
-
-val smallPieces = listOf(Tile.SHIP_S1, Tile.SHIP_S2)
-val medPieces = listOf(Tile.SHIP_M1, Tile.SHIP_M2, Tile.SHIP_M3)
-val largePieces = listOf(Tile.SHIP_L1, Tile.SHIP_L2, Tile.SHIP_L3, Tile.SHIP_L4)
-val allPieces = listOf(Tile.SHIP_S1, Tile.SHIP_S2, Tile.SHIP_M1, Tile.SHIP_M2, Tile.SHIP_M3,
-    Tile.SHIP_L1, Tile.SHIP_L2, Tile.SHIP_L3, Tile.SHIP_L4)
+import androidx.navigation.Navigation
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val newGameButton = findViewById<Button>(R.id.newGameButton)
-        newGameButton.setOnClickListener {
-            initGame()
-        }
-        initGame()
-    }
-
-    private fun initGame() {
-        val state = GameState()
-        val table = findViewById<TableLayout>(R.id.gridTable)
-        for (i in 0..7) {
-            val row = table.getChildAt(i) as TableRow
-            for (j in 0..7) {
-                val tile = row.getChildAt(j) as ImageView
-                tile.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.tile_unrevealed))
-                tile.rotation = 0F
-                tile.setOnClickListener {
-                    showTile(i,j,tile,state)
-                    revealTile(i,j,state)
-                }
-            }
-        }
-        val ship2icon = findViewById<ImageView>(R.id.ship2Icon)
-        ship2icon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_ship_2))
-        val ship3icon = findViewById<ImageView>(R.id.ship3Icon)
-        ship3icon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_ship_3))
-        val ship4icon = findViewById<ImageView>(R.id.ship4Icon)
-        ship4icon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_ship_4))
-        val text = findViewById<TextView>(R.id.textView)
-        text.setText(R.string.app_name)
-    }
-
-    // revealTile handles gameplay logic of revealing a tapped tile
-    private fun revealTile(i : Int, j : Int, state : GameState) {
-        when (state.gridContents[i][j]) {
-            Tile.MINE -> endGame(state, false)
-            in allPieces -> state.foundPieces.add(state.gridContents[i][j])
-        }
-        if (state.foundPieces.containsAll(smallPieces)) {
-            val ship2icon = findViewById<ImageView>(R.id.ship2Icon)
-            ship2icon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_ship_destroyed))
-        }
-        if (state.foundPieces.containsAll(medPieces)) {
-            val ship3icon = findViewById<ImageView>(R.id.ship3Icon)
-            ship3icon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_ship_destroyed))
-        }
-        if (state.foundPieces.containsAll(largePieces)) {
-            val ship4icon = findViewById<ImageView>(R.id.ship4Icon)
-            ship4icon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_ship_destroyed))
-        }
-        if (state.foundPieces.containsAll(allPieces)) {
-            endGame(state, true)
-        }
-    }
-
-    // showTile handles graphics only, is used when tapped and on showing full board on win/loss
-    private fun showTile(i : Int, j : Int, tile : ImageView, state : GameState) {
-        tile.setImageDrawable(ContextCompat.getDrawable(this, state.gridContents[i][j].image))
-        when (state.gridContents[i][j]) {
-            in smallPieces -> tile.rotation = state.ship2Ori * 90F
-            in medPieces -> tile.rotation = state.ship3Ori * 90F
-            in largePieces -> tile.rotation = state.ship4Ori * 90F
-        }
-    }
-
-    private fun endGame(state : GameState, win : Boolean) {
-        val table = findViewById<TableLayout>(R.id.gridTable)
-        for (i in 0..7) {
-            val row = table.getChildAt(i) as TableRow
-            for (j in 0..7) {
-                val tile = row.getChildAt(j) as ImageView
-                showTile(i,j,tile,state)
-                tile.setOnClickListener(null)
-            }
-        }
-        val text = findViewById<TextView>(R.id.textView)
-        text.setText(if (win) R.string.win else R.string.lose)
     }
 }
